d399eeabb51a80e25c5af257f47561fd7cd89da6
elyssadonato <142195541+elyssadonato@users.noreply.github.com>
2023-09-05 16:44:01 -0700

    Built JavaScript to make the select dropdown interactive

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ function updateCurrentTime() {
   phoenixTimeElement.innerHTML = currentPhoenixTime.format(
     "h:mm:ss [<small>]A[<small>]"
   );
+  let manilaElement = document.querySelector("#manila");
+  let manilaDateElement = manilaElement.querySelector(".date");
+  let manilaTimeElement = manilaElement.querySelector(".time");
+  let currentManilaTime = moment().tz("Asia/Manila");
+  manilaDateElement.innerHTML = currentManilaTime.format("MMMM Do, YYYY");
+  manilaTimeElement.innerHTML = currentManilaTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
   let romeElement = document.querySelector("#rome");
   let romeDateElement = romeElement.querySelector(".date");
   let romeTimeElement = romeElement.querySelector(".time");
@@ -15,6 +23,50 @@ function updateCurrentTime() {
   romeTimeElement.innerHTML = currentRomeTime.format(
     "h:mm:ss [<small>]A[<small>]"
   );
+  let seoulElement = document.querySelector("#seoul");
+  let seoulDateElement = seoulElement.querySelector(".date");
+  let seoulTimeElement = seoulElement.querySelector(".time");
+  let currentSeoulTime = moment().tz("Asia/Seoul");
+  seoulDateElement.innerHTML = currentSeoulTime.format("MMMM Do, YYYY");
+  seoulTimeElement.innerHTML = currentSeoulTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let currentSydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = currentSydneyTime.format("MMMM Do, YYYY");
+  sydneyTimeElement.innerHTML = currentSydneyTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+  let currentBerlinTime = moment().tz("Europe/Berlin");
+  berlinDateElement.innerHTML = currentBerlinTime.format("MMMM Do, YYYY");
+  berlinTimeElement.innerHTML = currentBerlinTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+}
+
+function showCurrentCityTime(event) {
+  let timeZone = event.target.value;
+  let cityName = timeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(timeZone);
+  let cityElement = document.querySelector("#cities");
+  cityElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+       <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+   </div>`;
 }
 updateCurrentTime();
 setInterval(updateCurrentTime);
+
+let citiesSelectElement = document.querySelector("#select");
+citiesSelectElement.addEventListener("change", showCurrentCityTime);
