9d1a164068066783d05e340798b2046a029011e4
elyssadonato <142195541+elyssadonato@users.noreply.github.com>
2023-09-05 15:09:41 -0700

    Integrated JavaScript functions to display current times

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-
+function updateCurrentTime() {
+  let phoenixElement = document.querySelector("#phoenix");
+  let phoenixDateElement = phoenixElement.querySelector(".date");
+  let phoenixTimeElement = phoenixElement.querySelector(".time");
+  let currentPhoenixTime = moment().tz("America/Phoenix");
+  phoenixDateElement.innerHTML = currentPhoenixTime.format("MMMM Do, YYYY");
+  phoenixTimeElement.innerHTML = currentPhoenixTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+  let romeElement = document.querySelector("#rome");
+  let romeDateElement = romeElement.querySelector(".date");
+  let romeTimeElement = romeElement.querySelector(".time");
+  let currentRomeTime = moment().tz("Europe/Rome");
+  romeDateElement.innerHTML = currentRomeTime.format("MMMM Do, YYYY");
+  romeTimeElement.innerHTML = currentRomeTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+}
+updateCurrentTime();
+setInterval(updateCurrentTime);
